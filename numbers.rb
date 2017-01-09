f4a57ea8e99b814f1e1eb68938f4f90d0c69b4b2
Timy90 <sy.thomas.pro@gmail.com>
2017-01-09 14:36:35 +0100

    add v1

---
+++ numbers.rb
@@ -0,0 +1,13 @@
+#Exercices sur les nombres
+
+#1. calculez le nombre de seconde dans une année
+
+calcul = 365 * 24 * 60 * 60
+
+puts calcul
+
+#2. calculez ton age en seconde
+
+calcul = 35 * 365 * 24 * 60 * 60
+
+puts calcul
