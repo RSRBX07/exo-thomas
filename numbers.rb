b80e973bc65a55e7c08d65099b64d558aa6e056b
Timy90 <sy.thomas.pro@gmail.com>
2017-01-09 16:10:02 +0100

    add version

--- numbers.rb
+++ numbers.rb
@@ -6,12 +6,21 @@ calcul = 365 * 24 * 60 * 60
 
 puts calcul
 
+
 #2. calculez ton age en seconde
 
 now = Time.now
 
-btime = Time.new "1981/06/02"
+btime = Time.new 1981,06,02
 
 age_in_sec = now - btime
 
 puts age_in_sec
+
+
+#3 Origine de Time
+
+origin = Time.new 0
+
+puts Time.now.to_i / calcul
+
