63ed7c170e7f7068eede029c8d51439e2b004254
Timy90 <sy.thomas.pro@gmail.com>
2017-01-10 15:08:11 +0100

    add date version

--- loto.rb
+++ loto.rb
@@ -7,3 +7,15 @@ tirage = loto.shuffle.take 5
 day = tirage.sort
 
 puts "le tirage du jour est #{day}"
+
+# affichage de la cagnote entre 100 et 500k€
+# le vendredi 13, la cagonte est de 2 millions
+
+is_vendredi_13 = date.today
+if is_vendredi_13
+  cagnote = 2000000
+else
+  cagnote = rand(100000..500000)
+end
+
+puts "la cagnote du jour est de  #{cagnote} €"
