a22eaeb770d0c530399e1b4ca4cfb15d24016c72
Timy90 <sy.thomas.pro@gmail.com>
2017-01-10 09:08:46 +0100

    add version

---
+++ dice.rb
@@ -0,0 +1,7 @@
+# affiche le tirage d'un dés à 6 faces
+
+faces = [1,2,3,4,5,6]
+
+roll = faces[rand 6]
+
+puts roll
---
+++ loto.rb
@@ -0,0 +1,7 @@
+# affiche le tirage d'un loto
+
+loto = [rand(1..45), rand(1..45), rand(1..45), rand(1..45), rand(1..45)]
+
+day = loto
+
+puts "le tirage du jour est #{day}"
