3155611d132dddbb42c56e00f3fb472b9973abbc
Timy90 <sy.thomas.pro@gmail.com>
2017-01-10 16:29:22 +0100

    add song.rb

---
+++ song.rb
@@ -0,0 +1,24 @@
+=begin
+
+Ma poule n'a plus qu'vingt-neuf poussins
+Ma poule n'a plus qu'vingt-neuf poussins
+Elle en avait trente
+Allongeons la jambe !
+
+Refrain
+Allongeons la jambe, la jambe
+Car la route est longue !
+Allongeons la jambe, la jambe
+Car la route est longue !
+
+Ma poule n'a plus qu'un seul poussin
+Ma poule n'a plus qu'un seul poussin
+Elle en avait trente
+Allongeons la jambe !
+
+Refrain
+
+=end
+
+text = "Ma poule n'a plus #{number} "
+puts 
