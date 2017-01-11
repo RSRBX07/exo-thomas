baea11a3d9398b3d3e4fcc2649aed55bc3419fc7
Timy90 <sy.thomas.pro@gmail.com>
2017-01-11 16:07:33 +0100

    add version
    Method roll_f6

--- dice.rb
+++ dice.rb
@@ -1,7 +1,14 @@
-# affiche le tirage d'un dés à 6 faces
+# roll method
+def roll_f6
+   # affiche le tirage d'un dés à 6 faces
 
-faces = [1,2,3,4,5,6]
+    faces = [1,2,3,4,5,6]
 
-roll = faces[rand 6]
+    roll = faces[rand 6]
 
-puts roll
+    return roll 
+end
+
+
+
+puts roll_f6
