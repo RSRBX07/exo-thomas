3d8d2ef3204ed322cc4bb691266a9c99be034874
Timy90 <sy.thomas.pro@gmail.com>
2017-01-12 15:54:45 +0100

    add version

--- french_number.rb
+++ french_number.rb
@@ -2,31 +2,12 @@
 # - qui prend une instance de Fixnum en argument (accepte de 0 à 9)
 # - qui retourne la chaine de caractere du chiffre en toutes lettres
 
-def french_number num
-  case num
-  when 0
-    "zero"
-  when 1
-    "un"
-  when 2
-    "deux"
-  when 3
-    "trois"
-  when 4
-    "quatre"
-  when 5
-    "cinq"
-  when 6
-    "six"
-  when 7
-    "sept" 
-  when 8
-    "huit"
-  when 9
-    "neuf"
-  else
-    nil            
-  end
+def french_number index
+  chiffre = ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf"]
+  chiffre[index]
 end  
 
-puts french_number 5  
+puts "Donnez un chiffre"
+index = gets.to_i
+
+puts french_number index
