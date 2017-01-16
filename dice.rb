73c8d9218a95fb4ed3b607b5d05d2cea4a308b77
Timy90 <sy.thomas.pro@gmail.com>
2017-01-16 15:50:09 +0100

    add version

--- dice.rb
+++ dice.rb
@@ -1,19 +1,11 @@
-# roll + roll_cheated method
-def roll_f6 (cheated_value = nil)
-roll_cheated = 1 + rand(6)
-  if cheated_value =! nil
-    # Valeur du dés pipé
-    cheated_value = roll  
-  end
-# dés à 6 faces
-roll = 1 + rand(6)    
-end
-
-puts "Dés pipés oui/non"
-choise= gets.chomp
-
-#puts roll_f6
-
+# Classe Dice
 class Dice
-  
+
+  # roll method
+  def roll (cheat = nil) 
+    return cheat if cheat 
+    dice = 1 + rand(6)
+  end
+
 end
+
