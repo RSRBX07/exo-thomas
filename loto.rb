2011ba468f5a6e67f0124fa9e23995abacbdc74a
Timy90 <sy.thomas.pro@gmail.com>
2017-01-16 15:51:22 +0100

    add version
    Classe Loto

--- loto.rb
+++ loto.rb
@@ -1,36 +1,77 @@
-# demander une grille de jeu
-puts "Entrer votre grille du loto "
-grille = gets.chomp
+# Objet date
+require 'date'
 
-# affiche le tirage d'un loto
+# Classe Loto
+class Loto
+  
+ def self.get_grid
+    grid = []
+    5.times do
+      input = gets.to_i
+      grid << input
+    end
+    grid
+  end
 
-loto = (1..45).to_a
+  def self.get_flash
+    (1..45).to_a.shuffle.take 5
+  end
 
-tirage = loto.shuffle.take 5
+  def has_winner?
+    #comprer tous les bulletins valides avec la grille gagnante
+    sorted_draw = draw.sort
+    @saved_grids.each do |grid|
+      sorted_grid = grid.sort
+      return true if sorted_grid == sorted_draw
+    end
+    return false
+  end
 
-day = tirage.sort
+  # enregistre une grille
+  # pour le loto courant
+  def validate_grid grid
+    # verifier que le tirage à pas eu lieu
+    if @picked_balls.empty?
+      @saved_grids ||= []
+      @saved_grids << grid
+    else
+      false
+    end
+  end 
 
-puts "le tirage du jour est #{day}"
+  # affichage de la cagnotte entre 100 et 500k€
+  # le vendredi 13, la cagontte est de 2 millions
 
-# affichage de la cagnote entre 100 et 500k€
-# le vendredi 13, la cagonte est de 2 millions
+  def vendredi_13?
+    Date.today.day == 13 and Date.today.friday?
+  end
 
-is_vendredi_13 = false
-if is_vendredi_13
-  cagnote = 2000000
-else
-  cagnote = rand(100000..500000)
+  def prize
+    cagnotte = if vendredi_13?
+        2_000_000
+      else
+        100_000
+      end
+    puts "Le montant de la cagnote du jour est de #{cagnotte}"
+    cagnotte
+  end
+
+  def draw
+    available_balls = (1..45).to_a
+    # shuffle balls and take 5
+    @picked_balls ||= available_balls.shuffle.take(5)
+    
+    puts "Le tirage du jour est : #{@picked_balls.sort}" 
+    @picked_balls
+  end
+
+  def check_grid grid
+    # afficher si gagne ou perdu
+    if grid.sort == draw.sort
+      puts "You win !"
+    else
+      puts "You loose !"
+    end
+  end
+
 end
-
-puts "la cagnote du jour est de  #{cagnote} €"
-
-# afficher si gagner ou perdu
-
-puts "#{grille}"
-
-if grille = true 
-  puts "Gagner"
-else
-  puts "Perdu"
-end
-
