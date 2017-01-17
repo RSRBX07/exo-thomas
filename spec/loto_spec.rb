69e97dd034a639c3335bd28c0a4cb480c21b3584
Timy90 <sy.thomas.pro@gmail.com>
2017-01-17 16:57:18 +0100

    add version
    RSpec

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -1,10 +1,26 @@
 #Test Loto
 # appel la classe Loto
-require './loto.rb'
+require_relative '../loto/loto.rb'
 
-my_loto = Loto.new
-my_loto.validate_grid Loto.get_flash
-my_loto.has_winner?
-puts "Le tirage du jour est : #{my_loto.picked_balls.sort}" 
-puts "La grille est : #{Loto.get_flash.sort}"
-my_loto.check_grid (Loto.get_flash)
+RSpec.describe Loto do
+  xit 'gets grids' do
+    Loto.get_grid
+  end
+
+  it 'give a radom flash grid' do
+    expect(Loto.get_flash).not_to be_nil
+    expect(Loto.get_flash).to be_a Array
+    expect(Loto.get_flash.size).to equal 5
+    expect(Loto.get_flash).not_to eql Loto.get_flash
+  end
+
+  xit 'make a draw' do
+    loto = Loto.new
+    loto_draw = loto.draw
+    expect(loto).to respond_to :draw
+    expect(loto_draw).not_to be_nil
+    expect(loto_draw).to be_a Array
+    expect(loto_draw.size).to equal 5
+    expect(loto_draw).not_to eql loto.draw
+  end
+end  
