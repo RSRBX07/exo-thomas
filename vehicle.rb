3ec06005a390ee12af5e06d726a28473e8fa70f2
Timy90 <sy.thomas.pro@gmail.com>
2017-01-19 11:38:44 +0100

    add version
    count

--- vehicle.rb
+++ vehicle.rb
@@ -3,6 +3,11 @@ class Vehicle
  attr_reader :position
  attr_reader :out_of_order
 
+ def self.count
+   cpt = 0
+   add cpt =+ 1
+ end
+
  def initialize
    @position = :roubaix
    @out_of_order = false
