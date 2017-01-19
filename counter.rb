5893a91bd7b0c9d83d00342fa2ebb002ff4c2b30
Timy90 <sy.thomas.pro@gmail.com>
2017-01-19 16:33:08 +0100

    add version

---
+++ counter.rb
@@ -0,0 +1,23 @@
+class Counter
+  
+  def add_one
+    @read_value = value +1
+    File.open file_path, "w" do |Counter_file|
+      Counter_file.write @read_value
+    end
+  end
+
+  def value
+    @read_value ||= File.open file_path, "r" do |counter_file|
+      Counter_file.each_line {|line| return line.to_i}
+    end
+  end
+
+
+private
+  def file_path
+    dirname = File.join(File.dirname(__FILE__), '../tmp/')
+    dirname + 'counter.txt'
+  end
+
+end
--- vehicle.rb
+++ vehicle.rb
@@ -1,21 +1,4 @@
-class Counter
-  path_file = "./tmp/counter.txt"
-  
-  def add_one
-    new_val = value +1
-    File.open path_file, "w" do |Counter_file|
-      Counter_file.write new_val
-    end
-  end
-
-  def value
-    File.open path_file, "r" do |Counter_file|
-      Counter_file.each_line {|line| return line.to_i}
-    end
-  end
-end
-
-
+require_relative './counter'
 
 class Vehicle
 
@@ -29,7 +12,7 @@ class Vehicle
  def initialize
    @position = :roubaix
    @out_of_order = false
-   Counter..new.add_one
+   Counter.new.add_one
  end
 
  def move destination
