9a5f52fb8d1efccd1ab594b608287be3d75b2883
Timy90 <sy.thomas.pro@gmail.com>
2017-01-23 16:47:37 +0100

    add player to Game

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123135022) do
+ActiveRecord::Schema.define(version: 20170123154309) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "players"
   end
 
 end
