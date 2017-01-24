dc8518cfdadac3d49fba0698793b79512f0d5bfe
Timy90 <sy.thomas.pro@gmail.com>
2017-01-24 16:54:21 +0100

    add draw
    add draw games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123154309) do
+ActiveRecord::Schema.define(version: 20170124154012) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170123154309) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "players"
+    t.string   "draw"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Game.destroy_all
-Game.create([{ name: 'loto du Lundi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985}, 
-{ name: 'loto du Mardi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985}, 
-{ name: 'loto du Mercredi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985},
-{ name: 'loto du Jeudi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985},
-{ name: 'loto du Vendredi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985}])
+Game.create([{ name: 'loto du Lundi', description: "Le tirage du Lundi", players: 2245985, draw:''}, 
+{ name: 'loto du Mardi', description: "Le tirage du Mardi", players: 2245985, draw:''}, 
+{ name: 'loto du Mercredi', description: "Le tirage du Mercredi", players: 2245985, draw:''},
+{ name: 'loto du Jeudi', description: "Le tirage du Jeudi", players: 2245985, draw:''},
+{ name: 'loto du Vendredi', description: "Le tirage du Vendredi", players: 2245985, draw:''}])
