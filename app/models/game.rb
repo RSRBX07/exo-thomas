9a9397c308970589e8144eb9d3bbc521ae5540b5
Timy90 <sy.thomas.pro@gmail.com>
2017-01-24 17:01:54 +0100

    add version draw

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,4 +9,11 @@ class Game < ApplicationRecord
     @players -= 1
   end
 
+  def draw
+    available_balls = (1..45).to_a
+    # shuffle balls and take 5
+    @picked_balls = available_balls.shuffle.take(5)
+    @picked_balls
+  end
+
 end
