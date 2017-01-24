ab6a0fdb8d8eabb9849681c13fb446d82962d3fe
Timy90 <sy.thomas.pro@gmail.com>
2017-01-24 11:29:50 +0100

    add version
    add seeds loto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.destroy_all
+Game.create([{ name: 'loto du Lundi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985}, 
+{ name: 'loto du Mardi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985}, 
+{ name: 'loto du Mercredi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985},
+{ name: 'loto du Jeudi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985},
+{ name: 'loto du Vendredi', description: "Le tirage est 12, 42, 11, 6, 33", players: 2245985}])
